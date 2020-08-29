05b1a75560d0306036b58d88b4a239f4b3e059b1
CapRap <kevinmli@seas.upenn.edu>
2020-08-29 05:24:58 -0400

    updated settings.py to original

--- capitalrap/settings.py
+++ capitalrap/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR + '/edgarapp/', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # To access filings
 STATICFILES_DIRS = [
-    ('filings', '../../mnt/filings/files/'),
+    ('filings', '/mnt/filings/files/'),
 ]
